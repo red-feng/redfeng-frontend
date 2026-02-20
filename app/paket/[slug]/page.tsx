a45f8f27a82ecd5445a31c67bb3fcbcaf421f852
red-feng <jenovacinfinityroyal@gmail.com>
2026-02-21 01:14:32 +0700

    fix fetch syntax

--- app/paket/[slug]/page.tsx
+++ app/paket/[slug]/page.tsx
@@ -1,49 +1,27 @@
-import Link from "next/link";
 import { notFound } from "next/navigation";
 
-export const revalidate = 300;
-
-
+export const revalidate = 300; // cache 1 menit
 
 async function getData(slug: string) {
-  console.log("SLUG FROM PARAMS:", slug);
-
   const res = await fetch(
     `https://redfeng.co/wp-json/redfeng/v1/paket/${slug}`,
-    { cache: "no-store" }
+    {
+      next: { revalidate: 300 },
+    }
   );
 
-  console.log("FETCH STATUS:", res.status);
+  if (!res.ok) return null;
 
-  if (!res.ok) {
-    console.log("FETCH FAILED");
-    return null;
-  }
-
-  const data = await res.json();
-  console.log("DATA EXISTS:", !!data);
-
-  return data;
+  return res.json();
 }
+
 
 export default async function CheckoutPage({
   params,
-  searchParams,
 }: {
-  params: { slug?: string };
-  searchParams: { nxtPslug?: string };
+  params: { slug: string };
 }) {
-  const slug = params.slug || searchParams.nxtPslug;
-
-  console.log("FINAL SLUG:", slug);
-
-  if (!slug) return notFound();
-console.log("PARAMS OBJECT:", params);
-console.log("SLUG VALUE:", slug);
-  const data = await getData(slug);
-
-console.log("DATA SERVER:", data);
-
+  const data = await getData(params.slug);
 
   if (!data) return notFound();
 
@@ -51,14 +29,13 @@ console.log("DATA SERVER:", data);
   const adminFee = publicPrice * 0.03;
   const subtotal = publicPrice + adminFee;
   const ppn = subtotal * 0.11;
-  const total = Math.round(subtotal + ppn);
-
+  const total = subtotal + ppn;
 
   return (
     <main className="min-h-screen bg-gray-100 p-10">
       <div className="max-w-3xl mx-auto bg-white p-8 rounded-xl shadow">
         <h1 className="text-3xl font-bold mb-6">
-        Detail Paket
+          Checkout
         </h1>
 
         <div className="space-y-3 mb-8">
@@ -83,12 +60,6 @@ console.log("DATA SERVER:", data);
             <span>Total</span>
             <span>Rp {total.toLocaleString("id-ID")}</span>
           </div>
-    <Link
-  href={`/checkout/${slug}`}
-  className="block w-full bg-blue-600 text-white text-center py-3 rounded-lg mt-6"
->
-  Lanjut ke Booking
-</Link>
         </div>
       </div>
     </main>
