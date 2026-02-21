72ce9f4efb709504a3cc8e3c51359fd9fb5786b7
red-feng <jenovacinfinityroyal@gmail.com>
2026-02-22 03:59:21 +0700

    refactor admin protected routes

--- app/admin/(protected)/layout.tsx
+++ app/admin/(protected)/layout.tsx
@@ -1,21 +1,11 @@
 import { redirect } from "next/navigation"
 import { createClient } from "@/lib/supabase/server"
-import { headers } from "next/headers"
 
-export default async function AdminLayout({
+export default async function AdminProtectedLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-
-  const headerList = await headers()
-  const pathname = headerList.get("x-invoke-path") || ""
-
-  // 🚫 Jangan proteksi halaman login
-  if (pathname.includes("/admin/login")) {
-    return <>{children}</>
-  }
-
   const supabase = await createClient()
 
   const {
