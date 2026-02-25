9c706fde9d35e36d57c302b51ed3331426002a8c
red-feng <jenovacinfinityroyal@gmail.com>
2026-02-25 14:27:04 +0700

    Fix API

--- app/api/packages/route.ts
+++ app/api/packages/route.ts
@@ -3,6 +3,7 @@ import { createClient } from "@supabase/supabase-js"
 import type { NextRequest } from "next/server"
 
 export async function GET(request: NextRequest) {
+
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!
