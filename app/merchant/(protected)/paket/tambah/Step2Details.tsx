a5a928793f0a7096d545fee8219a80ea5418c09b
red-feng <jenovacinfinityroyal@gmail.com>
2026-02-28 22:50:20 +0700

    Fix package

--- app/merchant/(protected)/paket/tambah/Step2Details.tsx
+++ app/merchant/(protected)/paket/tambah/Step2Details.tsx
@@ -29,9 +29,9 @@ export default function Step2Details({
 </div>
 
       {/* CONTENT WRAPPER */}
-      <div className="flex justify-center px-8 pb-28">
+      <div className="flex justify-center px-6 pb-20">
 
-        <div className="w-full max-w-5xl bg-white rounded-3xl shadow-[0_20px_50px_rgba(0,0,0,0.15)] p-14">
+        <div className="w-full max-w-3xl bg-white rounded-3xl shadow-[0_20px_50px_rgba(0,0,0,0.15)] p-14">
 
           <h1 className="text-2xl font-bold mb-1">
             Buat Paket Baru
