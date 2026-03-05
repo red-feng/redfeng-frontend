554de28673466802e921a8e9eee657f6ab3855f0
red-feng <jenovacinfinityroyal@gmail.com>
2026-03-05 15:19:23 +0700

    Fix detile package

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,11 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "RedFeng Travel",
   description: "RedFeng Digital Travel Ecosystem",
+  icons: {
+    icon: "/logo-redfeng.png",
+    shortcut: "/logo-redfeng.png",
+    apple: "/logo-redfeng.png",
+  },
 };
 
 export default function RootLayout({
