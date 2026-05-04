e2601ab9b230c639d9c6a0bf58f2e6a1f6153907
red-feng <jenovacinfinityroyal@gmail.com>
2026-05-04 11:34:53 +0700

    fix all

--- app/components/home/homeContent.tsx
+++ app/components/home/homeContent.tsx
@@ -153,7 +153,7 @@ export const payments: PaymentLogo[] = [
   { label: "VISA", src: "/home-assets/payment-visa.svg", width: 68, height: 22, mobileRenderWidth: 50, desktopRenderWidth: 58 },
   { label: "Mastercard", src: "/home-assets/payment-mastercard.svg", width: 112, height: 22, mobileRenderWidth: 78, desktopRenderWidth: 90 },
   { label: "BCA", src: "/home-assets/payment-bca.png", width: 1011, height: 325, mobileRenderWidth: 56, desktopRenderWidth: 66 },
-  { label: "Mandiri", src: "/home-assets/payment-mandiri.png", width: 66, height: 22, mobileRenderWidth: 54, desktopRenderWidth: 62 },
+  { label: "Mandiri", src: "/home-assets/payment-mandiri.png", width: 66, height: 22, mobileRenderWidth: 70, desktopRenderWidth: 81 },
   { label: "BNI", src: "/home-assets/payment-bni.png", width: 76, height: 22, mobileRenderWidth: 54, desktopRenderWidth: 62 },
   { label: "BRI", src: "/home-assets/payment-bri.png", width: 54, height: 22, mobileRenderWidth: 42, desktopRenderWidth: 48 },
   { label: "Midtrans", src: "/home-assets/payment-midtrans.jpg", width: 550, height: 101, mobileRenderWidth: 72, desktopRenderWidth: 86 },
